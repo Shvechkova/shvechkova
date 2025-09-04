b33b90caf4d145194b4fb22394771391c821520c
Anastasia Shvechkova <anastasiashvechkova@gmail.com>
2025-09-04 08:35:12 +0400

    Удалены все файлы проекта: email.php, index.html, proj-bb.html, proj-dia.html, proj-monastir.html, proj-samara.html, project.html, css/style.min.css, js/main.js, js/main.min.js, а также все изображения и стили.

---
+++ public_html/js/main.js
@@ -0,0 +1,69 @@
+// const text = document.querySelector(".text1");
+// text.innerHTML = text.innerText
+// 	.split("")
+// 	.map(
+// 		(char, i) => `<span style="transform:rotate(${i * 10.3}deg)">${char}</span>`
+// 	)
+// 	.join("");
+
+
+// $(document).ready(function() {
+//     $('.menu-burger__header').click(function() {
+//         $('.menu-burger__header').toggleClass('open-menu');
+//         $('.header_menu').toggleClass('open-menu');
+//         $('.main').toggleClass('open-menu');
+//         $('.social').toggleClass('open-menu');
+       
+        
+       
+//     });
+    
+// });
+
+// $('.menu__list-item').on('click', function(){
+//     $('.header_menu').removeClass('open-menu'); 
+//     $('.main').removeClass('open-menu');
+//     $('.social').removeClass('open-menu');
+// });
+
+// $('.header__logo-text').on('click', function(){
+//     $('.header_menu').removeClass('open-menu'); 
+//     $('.main').removeClass('open-menu');
+//     $('.social').removeClass('open-menu');
+// });
+
+//  $(function () {
+//   new AOS.init();
+//  })
+
+// $(document).ready(function(){
+//     $('#contact-form').submit(function(){
+//             // var subject = $(this).find("#subject :selected").text();
+//             var name = $(this).find("#name").val();
+//             var email = $(this).find("#email").val();
+//             var message = $(this).find("#message").val();
+
+//             if(!!name && !!email && !!message){
+//                 $.ajax({
+//                     type: "POST",
+//                     url: "/email.php",
+//                      data: { "name": name,"email": email, "message": message},
+//                     cache: false,
+//                     success: function(){
+//                         $('.success').toggleClass('opens');
+//                     }
+//                 })
+//             }
+//         return false;
+//     });
+// });
+
+$(document).ready(function(){
+const text = document.querySelector(".text1");
+text.innerHTML = text.innerText
+	.split("")
+	.map(
+		(char, i) => `<span style="transform:rotate(${i *8.2}deg)">${char}</span>`
+	)
+	.join("");
+});
